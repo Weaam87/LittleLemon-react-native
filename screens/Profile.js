1038433fe52fe631ed13c09c3f1e9f4d804f3247
Weaam alagha <32608267+Weaam87@users.noreply.github.com>
2024-02-06 16:57:02 +0800

    Add profile image placeholder and buttons

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 
@@ -29,8 +29,6 @@ export default function ProfileScreen() {
     }
   };
 
-
-
   useEffect(() => {
     // Fetch the user data from AsyncStorage
     const fetchUserData = async () => {
@@ -52,11 +50,29 @@ export default function ProfileScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>First Name:</Text>
-      <Text style={styles.data}>{firstName}</Text>
+      <View style={styles.rowContainer}>
+        <View style={styles.imageContainer}>
+          <Image source={require('../assets/profile_icon.png')} style={styles.profileImage} />
+        </View>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.changeButton}>
+            <Text style={styles.changeButtonText}>Change</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.removeButton}>
+            <Text style={styles.removeButtonText}>Remove</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
 
+      <Text style={styles.text}>Personal information</Text>
+      <Text style={styles.label}>First Name:</Text>
+      <View style={styles.rectangularFrame}>
+        <Text style={styles.data}>{firstName}</Text>
+      </View>
       <Text style={styles.label}>Email:</Text>
-      <Text style={styles.data}>{email}</Text>
+      <View style={styles.rectangularFrame}>
+        <Text style={styles.data}>{email}</Text>
+      </View>
 
       <TouchableOpacity onPress={handleTextPress}>
         <Text style={styles.text}>Press me to reset onboarding</Text>
@@ -68,8 +84,25 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'flex-start', // Align items to the left
+    justifyContent: 'flex-start', // Align content at the top
+    padding: 16, // Add padding for spacing
+  },
+  rowContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    marginRight: 30,
+    width: 100,
+    height: 100,
+    borderRadius: 50, // To make it a circle
+    overflow: 'hidden', // Ensure the image stays within the circular container
+  },
+  profileImage: {
+    resizeMode: 'contain',
+    width: '90%',
+    height: '100%', // Ensure the image takes the full height of the container
   },
   label: {
     fontSize: 18,
@@ -79,12 +112,46 @@ const styles = StyleSheet.create({
   data: {
     fontSize: 16,
     color: '#808080',
-    marginBottom: 20,
+    margin: 8,
   },
   text: {
     fontSize: 24,
-    color: '#495E57',
+    color: '#333333',
     marginVertical: 8,
     fontWeight: 'bold',
+  },
+  rectangularFrame: {
+    width: '100%',
+    paddingHorizontal: 8,
+    borderColor: '#333333',
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    margin: 8,
+  },
+  changeButton: {
+    backgroundColor: '#495E57',
+    padding: 10,
+    marginRight: 20,
+    borderRadius: 5,
+  },
+  removeButton: {
+    borderColor: '#F4CE14',
+    borderWidth: 1,
+    padding: 10,
+    marginLeft: 5,
+    borderRadius: 5,
+  },
+  changeButtonText: {
+    color: '#F4CE14',
+    textAlign: 'center',
+    margin: 4,
+  },
+  removeButtonText: {
+    color: '#495E57',
+    textAlign: 'center',
+    margin: 4,
   },
 });
